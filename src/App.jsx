2f8db0e52410798d39772226523cd2e8126dbe35
hamna hassan <hamnahassanvm@gmail.com>
2024-01-01 23:37:31 +0530

    made winning done

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,60 @@
-
-// import { useState } from 'react';
-import './Styles.scss'
-import Board from './Compo/Board'
+import { useState } from "react";
+import "./Styles.scss";
+import Board from "./Compo/Board";
+import { calculateWinner } from "./winner";
 // import Prop from './Prop'
 
 function App() {
+  const [squares, setsquares] = useState(Array(9).fill(null));
+  const [isXnext, setisXnext] = useState(false);
+  const nextPlayer=isXnext ? 'X':'O'; //this is a derived value from State
+  const winner=calculateWinner(squares);
+  const statusMsg=winner ? `winner is ${winner}`:`next player is ${nextPlayer}`;
+  const handlesquareClick = (clickedPosition) => {
+    if (squares[clickedPosition] || winner) {
+      return; //makes it not to click again once clicked and stop if there is winnner
+    }
+    setsquares((currentsquares) => {
+      return currentsquares.map((squareVal, position) => {
+        if (clickedPosition == position) {
+          // return 'X';
+          return isXnext ? "X" : "O";
+        }
+        return squareVal;
+      });
+    });
+    setisXnext((currentisXnext) => !currentisXnext);
+  };
   // const [counter,setCounter]=useState(1);
-// let counter=1;//after usestate v dontneed  this
-// const onBtnClick=()=>{
+  // let counter=1;//after usestate v dontneed  this
+  // const onBtnClick=()=>{
   // setCounter(counter+1);
   // setCounter(currentCounter=>{
   //   return currentCounter+1;
   // })//the answer will be same  bt  this callback aproach isbetter
   // counter=counter+1;//after usestate v dontneed  this
-  // this will work onlyin plian js not in react,it wont update coz inreact 
+  // this will work onlyin plian js not in react,it wont update coz inreact
   // v hav to make changes as states
 
-
   return (
-    <div className='app'>
-     
-{/* <Prop name="ham" >
+    <div className="app">
+      {/* <Prop name="ham" >
   <div>jhadsh</div>
   <h1>dfhh</h1>
 </Prop>
 <Prop name="3"/>
 <Prop name="...."/>    */}
-<Board/>
-{/* <button onClick={()=>{console.log("hlo")}}>click</button> */}
-{/* <button onClick={(event)=>{console.log("hlo",event)}}>click</button> */}
-{/* gives syntaticevents */}
-{/* <button onClick={onBtnClick}>click</button>
+{/* <h1>Next player is {nextPlayer}</h1> */}
+<h1>{statusMsg}</h1>
+{/* //now this has access to state */}
+      <Board squares={squares} handlesquareClick={handlesquareClick} />
+      {/* <button onClick={()=>{console.log("hlo")}}>click</button> */}
+      {/* <button onClick={(event)=>{console.log("hlo",event)}}>click</button> */}
+      {/* gives syntaticevents */}
+      {/* <button onClick={onBtnClick}>click</button>
 <div>{counter}</div> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Compo/Board.jsx
+++ src/Compo/Board.jsx
@@ -1,29 +1,39 @@
+// import { useState } from "react"
+import Sqaure from "./Sqaure";
 
-import { useState } from "react"
-import Sqaure from "./Sqaure"
+const Board = ({squares, handlesquareClick}) => {
+  //         const[squares,setsquares]=useState(Array(9).fill(null))
+  //     const[isXnext,setisXnext]=useState(false);
+  //         const handlesquareClick=(clickedPosition)=>{
+  //                 if(squares[clickedPosition]){
+  //                         return;//makes it not to click again once clicked
+  //                 }
+  //                 setsquares((currentsquares)=>{
+  //                         return currentsquares.map((squareVal,position)=>{
+  //                         if(clickedPosition==position){
+  //                                 // return 'X';
+  //                                 return isXnext ? 'X':'O';
+  //                         }
+  //                         return squareVal;
+  //                         })
 
-const Board = () => {
-        const[squares,setsquares]=useState(Array(9).fill(null))
-        // const[squares,setsquares]=useState([]);
-        const handlesquareClick=(clickedPosition)=>{
-                setsquares((currentsquares)=>{
-                        return currentsquares.map((squareVal,position)=>{
-                        if(clickedPosition==position){
-                                return 'X';
-                        }
-                        return squareVal;
-                        })
-                                  });
-        }
-        const renderSquare=(position)=>{
-                return <Sqaure value={squares[position]} onClick={()=>handlesquareClick(position)}/>
-        }
-        
+  //                                   });
+  //                                   setisXnext((currentisXnext)=>!currentisXnext)
+  //         };//now board component can only have access squares,to make everything accessible to app compo,v paste this to app.jsx
+  const renderSquare = (position) => {
+    return (
+      <Sqaure
+        value={squares[position]}
+        onClick={() => handlesquareClick(position)}
+      />
+    );
+  };
+
   return (
     <div className="board">
-        <div className="board-row">
-{/* <Sqaure value={0}/> */}
-{/* [[<Sqaure value={squares[0]} onClick={()=>{
+      <div className="board-row">
+        {/* <Sqaure value={0}/> */}
+        {/* [[<Sqaure value={squares[0]} onClick={()=>{
         handlesquareClick(0)
 }}/>
 <Sqaure value={squares[1]} onClick={()=>{
@@ -32,23 +42,23 @@ const Board = () => {
 <Sqaure value={squares[2]} onClick={()=>{
         handlesquareClick(2)
 }}/>]]  to simplify this used renderfunc up*/}
-{renderSquare(0)}
-{renderSquare(1)}
-{renderSquare(2)}
-        </div>
-        <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
         {renderSquare(3)}
-{renderSquare(4)}
-{renderSquare(5)}
-        </div>
-        <div className="board-row">
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
         {renderSquare(6)}
-{renderSquare(7)}
-{renderSquare(8)}
-        </div>
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;
 //0,1,2...represents indexes of box numbers
